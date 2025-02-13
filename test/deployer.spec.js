495d2d6699bef17aa1980f96b736da47ccec527f
uiolee <22849383+uiolee@users.noreply.github.com>
2025-02-13 17:14:33 +0800

    WIP: daemon test. rebase later

--- test/deployer.spec.js
+++ test/deployer.spec.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const {writeFile, mkdir, rm} = require('node:fs/promises');
+const {writeFile, readFile, mkdir, rm} = require('node:fs/promises');
 const {expect} = require('chai');
 const sinon = require('sinon');
 const rewire = require('rewire');
 
-const _testDir = 'tmp/_test/';
 
 describe('deployer', () => {
   describe('dry-run', () => {
@@ -142,4 +141,94 @@ describe('deployer', () => {
       expect(fakeHexoSpawn.callCount).to.be.equal(0);
     });
   });
+
+  describe.only('daemon', () => {
+    let hexo, args;
+
+    const rsyncdConfig = 'test/rsyncd.test.conf';
+    const rsyncdSecret = 'test/rsyncd.test.secret';
+    const _testDir = 'tmp/_test/';
+    const module1Dir = _testDir + 'module1/';
+    const module2Dir = _testDir + 'module2/';
+    const passwordFile1 = _testDir + 'user1';
+    const passwordFile2 = _testDir + 'user2';
+    const rsyncdPidFile = _testDir + "rsyncd.test.pid"
+
+    const args1Default = {host: 'localhost', user: 'user1', root: ':module1/'};
+    const args2Default = {host: 'localhost', user: 'user2', root: ':module2/'};
+
+    const deployer = require('../lib/deployer');
+    const {exec, spawn} = require('node:child_process');
+
+    before(() => {
+      const users = {'user1': "password1", 'user2': 'password1'};
+      const startRsyncd = () => {
+        return new Promise((resolve, reject) => {
+          const child = spawn('rsync', ['--daemon', '--config', rsyncdConfig], {
+            detached: true,
+            stdio: 'ignore',
+          });
+          child.unref()
+          child.on('close', resolve);
+          child.on('exit', resolve);
+          child.on('error', reject);
+        }).then(((exitCode, signal) => {
+          if (exitCode !== 0) {
+            throw new Error(`childProcess: Failed to start rsyncd daemon, exit code: ${exitCode}. ${signal}`)
+          }
+        }))
+      }
+
+      return Promise.all([mkdir(module1Dir, {recursive: true}), mkdir(module2Dir, {recursive: true})]).then(() => {
+        return Promise.all([writeFile(passwordFile1, users['user1']), writeFile(passwordFile2, users['user2'])]).then(() => {
+          return Promise.all([spawn('chmod', ['600', passwordFile1]), spawn('chmod', ['600', passwordFile2]), spawn('chmod', ['600', rsyncdSecret])]).then(async () => {
+            return Promise.all([startRsyncd()])
+          })
+        });
+      });
+    });
+
+    after(() => {
+      const killRsyncd = () => {
+        return readFile(rsyncdPidFile).then((id) => {
+          return spawn('kill', ['-9', id.toString().trim()])
+        }).then((res) => {
+          return rm(rsyncdPidFile)
+        });
+      }
+
+      return killRsyncd().then(() => {
+        // rm(_testDir, {recursive: true})
+      })
+    });
+
+    beforeEach(() => {
+      hexo = {
+        public_dir: 'test'
+      };
+
+    });
+
+    afterEach(() => {
+    });
+
+    it('check daemon running', (done) => {
+      exec('ps aux | grep rsyncd | grep -v grep', (err, stdout, stderr) => {
+        if (err || stderr) {
+          done(err || stderr);
+        }
+
+        expect(stdout).to.include(`rsync --daemon --config ${rsyncdConfig}`);
+        done();
+      });
+    })
+
+    it("push file to module1", () => {
+      args = {...args1Default, args: "--port=11451 --password-file=" + passwordFile1};
+
+      return deployer.call(hexo, args).then(() => {
+        expect(true).to.be.true;
+      })
+    })
+  })
 });
